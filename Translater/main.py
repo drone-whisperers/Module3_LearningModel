124d2306f59001a775e97710ae8e3398493ae320
Dillon Claremont <dillon.claremont@icloud.com>
2020-03-16 17:11:33 -0400

    refactor

--- Translater/main.py
+++ Translater/main.py
@@ -3,5 +3,3 @@ from TranslaterCls import TranslaterCls
 translater = TranslaterCls()
 translater.translate_sentence("Big Jet 345 fly heading 353 ascend to flight level 170 speed 184 knots")
 
-
-print("Hello World")
